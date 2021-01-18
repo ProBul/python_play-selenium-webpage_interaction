cb023dd38089a374b208d63613c1ee434b0f5974
ProBul <bulkaisrael@gmail.com>
2021-01-18 10:11:44 +0200

    Update webpage_opener.py

--- webpage_opener.py
+++ webpage_opener.py
@@ -4,26 +4,25 @@ import time
 import logging
 from datetime import datetime
 
-logging.basicConfig(filename=r'C:\Users\Bulka\Documents\Python\Python_scripts\webpage_opener', level=logging.DEBUG,
+logging.basicConfig(filename=r'<PATH_TO_LOGFILE>', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
-#logger = logging.getLogger()
 
 try:
     logger.info("Starting the process")
     # Sets the path to the driver
-    PATH = r"C:\Users\Bulka\Documents\Python\chromedriver.exe"
+    PATH = r"<PATH_TO_WEBDRIVER_ON_YOUR_DRIVE>"
     driver = webdriver.Chrome(PATH)
 
     # Point the driver to the relevant web page
-    driver.get("https://www.torrentleech.org/")
+    driver.get("https://<URL>/")
 
     username = driver.find_element_by_name("username")  # Looks for the relevant element in the page source
-    username.send_keys("bulka20")  # places my input inside the element
+    username.send_keys("<USER_NAME/EMAIL>")  # places my input inside the element
     username.send_keys(Keys.TAB)  # Hit TAB once done
     time.sleep(3)
-    password = driver.find_element_by_name("password")
-    password.send_keys("TimeCube2020")
+    password = driver.find_element_by_name("password") # Search for element name "password" in the web page
+    password.send_keys("<PASSWORD HERE>")
     time.sleep(3)
     password.send_keys(Keys.RETURN)
     time.sleep(1)
